8de7a696ad7e2bda55adac71939291f3045f03f2
nev <snevedaren@gmail.com>
2017-11-07 23:41:35 +0700

    28: initial commit. does not compiles on linux because of p2(open/close).

---
+++ 28.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+const int NUM_COUNT = 100;
+
+int main()
+{
+    time_t now = time(NULL);
+   
+    if (now == (time_t)-1)
+    {
+        perror("time");
+        return EXIT_FAILURE;
+    }
+
+    srand((unsigned int) now);
+    FILE* fp[2];
+
+    if (p2open("sort -h", fp) == -1)
+    {
+        puts("p2open failed");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < NUM_COUNT; i++)
+    {
+        int num = rand() % 100;
+        
+        if (fprintf(fp[0], "%d\n", num) < 0)
+        {
+            perror("fprintf");
+            p2close(fp);
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (fputc(EOF, fp[0]) == EOF)
+    {
+        perror("fputc");
+        p2close(fp);
+        return EXIT_FAILURE;
+    }
+
+    char buffer[8];
+
+    while (fgets(buffer, 8, fp[1]))
+    {
+        puts(buffer);
+    }
+
+    if (!feof(fp[1]))
+    {
+        perror("fgets");
+        return EXIT_FAILURE;
+    }
+
+    if (p2close(fp) == -1)
+    {
+        puts("p2close failed");
+        return EXIT_FAILURE;
+    }
+}
